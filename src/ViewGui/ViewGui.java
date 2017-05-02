4b85d8c0265f12bfcd895a95724e58c27dc63ad3
Alwifaisal <faisalzulfikar05@gmail.com>
2017-05-02 07:36:32 -0700

    update

---
+++ src/ViewGui/ViewGui.java
@@ -0,0 +1,15 @@
+package ViewGui;
+
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author abdulnursahid
+ */
+public interface ViewGui {
+    public void addListener(ActionListener e);
+    public void ViewErrorMessage (String ErrorMessagge);
+    
+    
+    
+}
